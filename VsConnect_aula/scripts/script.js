f1ff176d60999d34c063261d9b1f461a74a1f5f0
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-06-02 21:50:12 -0300

    alteracao

--- VsConnect_aula/scripts/script.js
+++ VsConnect_aula/scripts/script.js
@@ -1,11 +1,27 @@
-let menu = document.getElementById("menu_links");
+let menu = document.getElementById("menu_links") //estou pedindo para identificar e buscar o menu_links
+let menu_barras =document.getElementById("menu_barras") // 02/06 
 
 function mostrarMenu() {
     if(window.getComputedStyle(menu).display == "none") {
-        // Mostre o menu
+        // Se estiver escondido 
         menu.style.display = "flex";
-    } else {
-        // Esconda o menu
+        // mostre o menu
+
+        menu_barras.setAttribute("aria_expanded","true") // 02/06 acessibilidade
+        menu_barras.setAttribute("aria_label","Fechar o menu") // 02/06 acessibilidade
+    } else { 
         menu.style.display = "none";
+        // Se nao esconda o menu
+
+        menu_barras.setAttribute("aria_expanded","false") // 02/06 acessibilidade
+        menu_barras.setAttribute("aria_label","Abrir o menu") // 02/06 acessibilidade
     }
 }
+
+addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        menu.style.display = "flex"
+    } else {
+        menu.style.display = "none"
+    }
+}); /* dia 02/06 adicionado  */
